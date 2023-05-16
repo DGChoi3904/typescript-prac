97abbf1ed73d0144dd6ebf21d0843f8d7d7237b9
DGChoi3904 <dgchoi3904@gmail.com>
2023-05-16 12:12:49 +0900

    컴파일 후의 코드 모습

---
+++ DG.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DG = void 0;
+function DG(a) {
+    var string = "";
+    return string;
+}
+exports.DG = DG;
--- test.js
+++ test.js
@@ -1,5 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var a = 1;
 var b = 2;
+// 실행만 하는 함수 추가
+function moreFunction() {
+    console.log("hi");
+}
 function c(a, b) {
     return a + b;
 }
